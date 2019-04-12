b8053505815353901115e93a499843771b899a57
ludde016 <ludde.swala@gmail.com>
2019-04-12 14:38:42 +0200

    Commit #1 (flags)
    Skapade en class för flag så jag kan skapa "flag" variabler och en flagmaker där jag ska skapa metoder för olika flaggor
    ¨

--- src/Flags/Flagmaker.java
+++ src/Flags/Flagmaker.java
@@ -1,13 +1,14 @@
 package Flags;
 
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 
 public class Flagmaker {
 	
 	public static Flag nordic(Color c) {
 		
 		Flag flag = new Flag();
-		Rectamgle back = new Rectangle();
+		Rectangle back = new Rectangle();
 		back.setHeight(300);
 		
 	}
