63e01764fcf1ba4cda16c229469bd1597372950f
ludde016 <ludde.swala@gmail.com>
2019-09-21 18:33:57 +0200

    JavaFX laboration commit #2
    Har gjort klart alla funktioner men måste lägga till knapparna i min grupp (root) så att de visas när man startar programmet

--- src/Uppgifter/LaborationJavaFX.java
+++ src/Uppgifter/LaborationJavaFX.java
@@ -2,12 +2,20 @@ package Uppgifter;
 
 import javafx.application.Application;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
-public class LaborationJavaFX extends Application {
+public class LaborationJavaFX extends Application implements EventHandler<ActionEvent> {
+
+	
+	Button change;
+	Button close;
+	Group root;
+	Scene scene;
 
 	public static void main(String[] args) {
 
@@ -16,24 +24,37 @@ public class LaborationJavaFX extends Application {
 
 	public void start(Stage primaryStage) throws Exception {
 
-		Group root = new Group();
-		Scene scene = new Scene(root, 500, 500, Color.SKYBLUE);
+		root = new Group();
+		scene = new Scene(root, 500, 500, Color.SKYBLUE);
 		primaryStage.setScene(scene);
 		primaryStage.show();
 
 	}
-
-	public void handle(ActionEvent event) {
-
-		// if(event.getSource()==buttonAdd){
-		// }
-		// else if(event.getSource()==buttonClear) {
-		// }
-		// else if(event.getSource()==buttonClose) {
-		//
-		// }
-		// else {
-		// return;
-		// }
+	
+public void handle(ActionEvent event) {
+		
+		if(event.getSource()==change){			
+		}
+		else if(event.getSource()==close) {
+			System.exit(0);
+		}
+		else {
+			return;
+		}
+		
+	}
+	
+	private void buttons() {
+		
+		change = new Button("Change");
+		close.setOnAction(this);
+		
+		close = new Button("Close");
+		close.setOnAction(this);
+		
+	}
+	
+	private void ChangeColor() {
+		root.setStyle("-fx-background-color: darkblue;");
 	}
 }
