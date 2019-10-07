746fa8c90afb5c32dd90fea192146bb938eac0b1
ludde016 <ludde.swala@gmail.com>
2019-10-07 10:58:12 +0200

    Miniräknare commit #4
    Skapade 2 textfields och gjorde en borderpane för att se till att textfielden inte hamnade i mina knappar

--- src/Miniraknare/Miniraknaren.java
+++ src/Miniraknare/Miniraknaren.java
@@ -3,47 +3,60 @@ package Miniraknare;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
-public class Miniraknaren extends Application{
+public class Miniraknaren extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		
-		HBox root = new HBox();
-		
-		//Skapa en input och output field
-		
-		//Skapa en metod som uppdaterar textfield
-		
-		GridPane numpad = new GridPane(); //Kod för numpad
 
-        String [] numpadKeys = {"1", "2", "3","+",
-                                "4", "5", "6","-",
-                                "7", "8", "9","*",
-                                "0", "c", "=","/", 
-                                "%", "\u221A"
-                                };
-        for (int i = 0; i < numpadKeys.length; i++) {
-            Button temp = new Button(numpadKeys[i]);
-            numpad.add(temp, i % 4, (int) Math.ceil(i/4)); // Vad betyder denna del? Math.ceil avrundar talet uppåt but why?
+		HBox root = new HBox(); //VBox fixar textfielden
 
-        } //Slut (numpad)
-        
-        root.getChildren().add(numpad);
-        
-        Scene scene = new Scene(root, 300, 300);
-        
-        primaryStage.setScene(scene);
+		// Skapa en metod som uppdaterar textfield
+
+		BorderPane PainBroder = new BorderPane();
+		root.getChildren().add(PainBroder);
+
+		GridPane numpad = new GridPane(); // Kod för numpad
+		
+
+		TextField inputField = new TextField();
+		inputField.setText("Label");
+		TextField outputField = new TextField();
+		outputField.setText("Label");
+		root.getChildren().addAll(inputField, outputField);
+		PainBroder.setTop(inputField);
+		PainBroder.setTop(outputField);
+
+		String[] numpadKeys = { "1", "2", "3", "+", "4", "5", "6", "-", "7", "8", "9", "*", "0", "c", "=", "/", "%",
+				"\u221A" };
+		
+		for (int i = 0; i < numpadKeys.length; i++) {
+			Button temp = new Button(numpadKeys[i]);
+			temp.setFont(Font.font("Verdana", 20));
+			temp.setPrefSize(1000, 1000);
+			numpad.add(temp, i % 4, (int) Math.ceil(i / 4));
+
+		} // Slut (numpad)
+
+		root.getChildren().add(numpad);
+		PainBroder.setCenter(numpad);
+
+		Scene scene = new Scene(root, 300, 300);
+
+		primaryStage.setScene(scene);
 		primaryStage.show();
-		
+
 	}
-	
+
 	public static void main(String[] args) {
-		
-		launch();	
-		}
+
+		launch();
+	}
 
 }
