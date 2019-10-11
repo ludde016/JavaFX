b04872afeb13e37cb6792daa4826f826e615fbed
ludde016 <ludde.swala@gmail.com>
2019-10-11 13:31:38 +0200

    Miniräknare commit #5
    Ändrade i min kod så att min BorderPane blev kodens root och VBoxen (som förut var en HBox) fick kallas displayField och läggas i toppen av min BorderPane. Detta fixade problemet där båda mina textfields la sig i varandra

--- src/Miniraknare/Miniraknaren.java
+++ src/Miniraknare/Miniraknaren.java
@@ -1,12 +1,13 @@
 package Miniraknare;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
@@ -15,37 +16,34 @@ public class Miniraknaren extends Application {
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 
-		HBox root = new HBox(); //VBox fixar textfielden
+		BorderPane root= new BorderPane();
+		
+		VBox displayField = new VBox();
 
 		// Skapa en metod som uppdaterar textfield
-
-		BorderPane PainBroder = new BorderPane();
-		root.getChildren().add(PainBroder);
 
 		GridPane numpad = new GridPane(); // Kod för numpad
 		
 
 		TextField inputField = new TextField();
-		inputField.setText("Label");
+		inputField.setText("Input");
 		TextField outputField = new TextField();
-		outputField.setText("Label");
-		root.getChildren().addAll(inputField, outputField);
-		PainBroder.setTop(inputField);
-		PainBroder.setTop(outputField);
+		outputField.setText("Output");
+		displayField.getChildren().addAll(inputField, outputField);
+		root.setTop(displayField);
 
-		String[] numpadKeys = { "1", "2", "3", "+", "4", "5", "6", "-", "7", "8", "9", "*", "0", "c", "=", "/", "%",
+		String[] numpadKeys = { "1", "2", "3", "+", "4", "5", "6", "-", "7", "8", "9", "*", "C", "0", "=", "/", "%",
 				"\u221A" };
 		
 		for (int i = 0; i < numpadKeys.length; i++) {
 			Button temp = new Button(numpadKeys[i]);
 			temp.setFont(Font.font("Verdana", 20));
-			temp.setPrefSize(1000, 1000);
+			temp.setPrefSize(100, 100);
 			numpad.add(temp, i % 4, (int) Math.ceil(i / 4));
 
 		} // Slut (numpad)
 
-		root.getChildren().add(numpad);
-		PainBroder.setCenter(numpad);
+		root.setCenter(numpad);
 
 		Scene scene = new Scene(root, 300, 300);
 
